e8cd3e935a021ef58db3e2e8801dab5acdf3d7d7
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-12-15 11:13:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6080388435').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "Movie_Star_File_Search_Bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "XDIK_bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
